a36fd49f9c665ef02b5b5f57973a9480557a4774
Levy Mateus Macedo <levymateusmacedo@gmail.com>
2023-09-05 21:25:55 -0300

    add Player.ts

---
+++ src/game-objects/Player.ts
@@ -0,0 +1,44 @@
+import { App } from "core/App";
+import Camera from "core/Camera";
+import { GameObject } from "core/GameObject";
+import Keyboard from "core/Keyboard";
+import { vec } from "utils";
+
+import type { MainShip } from "./MainShip";
+
+export const createPlayer = (createMainShip: MainShip): GameObject => {
+
+  const kgo = createMainShip(start, update);
+  const settings = App.settings.getKeyboardSettings();
+
+  // vars
+  const speed = vec(5, 5);
+  let move = vec(0, 0);
+
+  function start(o: GameObject) {
+    new Camera(o.root);
+  }
+
+  function update(dt: number) {
+
+    move = vec(0, 0);
+
+    if (Keyboard.isKeyDown(settings.MoveUp) && kgo.position.y >= Camera.MAX_CAMERA_Y) {
+      move.y = speed.y * -1;
+    }
+    if (Keyboard.isKeyDown(settings.MoveDown) && kgo.position.y <= Camera.MIN_CAMERA_Y) {
+      move.y = speed.y;
+    }
+    if (Keyboard.isKeyDown(settings.MoveLeft)) {
+      move.x = speed.x * -1;
+    }
+    if (Keyboard.isKeyDown(settings.MoveRight)) {
+      move.x = speed.x;
+    }
+
+    GameObject.move(kgo, move.x * dt, move.y * dt);
+  }
+
+  return kgo;
+
+}
