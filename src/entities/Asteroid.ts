0a23056fb22fa5bc0103dd69ebef00b1a558c94c
Levy Mateus Macedo <levymateusmacedo@gmail.com>
2023-10-05 15:02:43 -0300

    add fast asteroid

--- src/entities/Asteroid.ts
+++ src/entities/Asteroid.ts
@@ -1,28 +1,45 @@
 import GameObject from "core/GameObject";
-import { AnimatedSprite, Assets, Circle, Sprite } from "pixi.js";
-import { isValidCollisor, randf } from "utils/utils";
+import { AnimatedSprite, Assets, Sprite } from "pixi.js";
+import { dice, isValidCollisor, randf } from "utils/utils";
 import Entities from "./Entities";
+
+enum AsteroidType {
+  DEFAULT = 0,
+  FAST,
+}
 
 export class Asteroid extends GameObject {
 
-  public baseSprite: Sprite;
-  public explodeSprite: AnimatedSprite;
-  public collisionShape: Circle;
+  public type: AsteroidType = AsteroidType.DEFAULT;
+  private baseSprite: Sprite;
+  private explodeSprite: AnimatedSprite;
+  private flameSprite: AnimatedSprite | null = null;
 
   constructor(x?: number, y?: number) {
     super(Entities.ASTEROID);
     this.position.set(x, y);
     this.rotate = randf(0, 0.2);
-    this.speed.set(0, randf(0.4, 0.7));
+    this.speed.set(0, randf(0.5, 0.8));
     this.collisionShape.radius = 14;
+    this.type = dice(8).roll() >= 6 ? AsteroidType.FAST : this.type;
+    this.build();
+  }
+
+  private build(): void {
+    if (this.type === AsteroidType.FAST) {
+      this.speed.y = randf(2, 2.5);
+      this.addFlameSprite();
+    }
     this.addBaseSprite();
     this.addExplodeSprite();
     this.addListeners();
+    this.sortChildren();
   }
 
   private addBaseSprite(): void {
     this.baseSprite = Sprite.from(Assets.get('asteroid_base'));
     this.baseSprite.anchor.set(this.anchor);
+    this.baseSprite.zIndex = 0;
     this.addChild(this.baseSprite);
   }
 
@@ -32,8 +49,20 @@ export class Asteroid extends GameObject {
     this.explodeSprite.animationSpeed = this.speedAnimation;
     this.explodeSprite.anchor.set(this.anchor);
     this.explodeSprite.visible = false;
+    this.explodeSprite.zIndex = 0;
     this.explodeSprite.loop = false;
     this.addChild(this.explodeSprite);
+  }
+
+  private addFlameSprite(): void {
+    const spritesheet = Assets.get("asteroid_flame");
+    this.flameSprite = new AnimatedSprite(spritesheet.animations["flames"]);
+    this.flameSprite.animationSpeed = this.speedAnimation;
+    this.flameSprite.anchor.set(this.anchor);
+    this.flameSprite.zIndex = -1;
+    this.flameSprite.angle = 270;
+    this.flameSprite.play();
+    this.addChild(this.flameSprite);
   }
 
   private addListeners(): void {
@@ -48,7 +77,7 @@ export class Asteroid extends GameObject {
 
   private onUpdate(dt: number): void {
     this.position.y += this.speed.y * dt;
-    this.angle += this.rotate;
+    this.baseSprite.angle += this.rotate;
   }
 
   private onOutOfBounds(): void {
@@ -57,6 +86,7 @@ export class Asteroid extends GameObject {
 
   private explodeAndDestroy(): void {
     this.ticker.stop();
+    this.flameSprite?.visible ?? false;
     this.baseSprite.visible = false;
     this.explodeSprite.visible = true;
     this.explodeSprite.onComplete = () => {
