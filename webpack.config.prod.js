2ee2cd1b4afb5a52de83a2063b0c2e317d1c168f
Levy Mateus Macedo <levymateusmacedo@gmail.com>
2024-01-13 10:21:46 -0300

    build: json files optimize

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,3 +1,4 @@
+const JsonMinimizerPlugin = require("json-minimizer-webpack-plugin");
 const config = require(__dirname + '/webpack.config.js');
 const { merge } = require('webpack-merge');
 
@@ -5,5 +6,14 @@ module.exports = (props) => {
   return merge(config(props), {
     stats: 'verbose',
     mode: 'production',
+    optimization: {
+      minimize: true,
+      minimizer: [
+        new JsonMinimizerPlugin(),
+      ],
+      splitChunks: {
+        chunks: 'all',
+      },
+    },
   })
 }
